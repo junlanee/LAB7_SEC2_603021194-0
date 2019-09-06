441b03c90c134570cfc38c2dc67a6ebc7f746c0a
junlanee <junlanee@kkumail.com>
2019-09-06 17:11:38 +0700

    LAB7_SEC2_603021194-0

---
+++ Lab7SialogRV_Sec2_6030211940_new/app/src/main/java/com/example/lab7sialogrv_sec2_603021194_0_new/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab7sialogrv_sec2_603021194_0_new
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
